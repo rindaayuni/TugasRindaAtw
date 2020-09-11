dc594ed5e4b3924eac9b69e5b2c9428419ee3af0
rindaayuni <rindaayuni24@gmail.com>
2020-09-11 16:59:21 +0700

    penambahan kelas angsa

--- index.php
+++ index.php
@@ -11,5 +11,32 @@ $Kucing->jumlah_kaki = "4";
 $Kucing->bisa_terbang ="tidak";
 $Kucing->suara = "meong";
 
+echo "Kucing <br>";
 echo "Nama: $Kucing->nama";
+echo "<br>";
+echo "jumlah kaki: $Kucing->jumlah_kaki";
+echo "<br>";
+echo "bisa terbang: $Kucing->bisa_terbang";
+echo "<br>";
+echo "suara: $kucing->suara";
+echo "<hr>";
 
+class Angsa{
+	public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+}
+
+$Angsa = new Angsa;
+$Angsa->nama= "Soang";
+$Angsa->jumlah_kaki= "2";
+$Angsa->bisa_terbang= "tidak";
+$Angsa->suara= "kwaak";
+
+echo "Angsa <br>";
+echo "Nama: $Angsa->nama";
+echo "<br>";
+echo "Jumlah kaki $Angsa->jumlah_kaki";
+echo "<br>";
+echo "Bisa_terbang $Angsa->bisa_terbang";
+echo "<br>";
+echo "Suara $Angsa->suara";
+echo "<hr>";
